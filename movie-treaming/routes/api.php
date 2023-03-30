c4cd29c1291351b7fb937762a7e1e41cda1a3522
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2023-03-30 17:01:22 +0200

    add api rout list

--- movie-treaming/routes/api.php
+++ movie-treaming/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ActorController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\RatingController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +23,42 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+// Actor routes
+Route::get('/actors', [ActorController::class, 'index']);
+Route::post('/actors', [ActorController::class, 'store']);
+Route::put('/actors/{id}', [ActorController::class, 'update']);
+Route::delete('/actors/{id}', [ActorController::class, 'delete']);
+
+// Category routes
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+
+// Favorite routes
+Route::get('/favorites', [FavoriteController::class, 'index']);
+Route::post('/favorites', [FavoriteController::class, 'store']);
+Route::delete('/favorites/{id}', [FavoriteController::class, 'delete']);
+
+// Home routes
+Route::get('/home', [HomeController::class, 'index']);
+
+// Movie routes
+Route::get('/movies', [MovieController::class, 'index']);
+Route::post('/movies', [MovieController::class, 'store']);
+Route::put('/movies/{id}', [MovieController::class, 'update']);
+Route::delete('/movies/{id}', [MovieController::class, 'delete']);
+
+// Rating routes
+Route::get('/ratings', [RatingController::class, 'index']);
+Route::post('/ratings', [RatingController::class, 'store']);
+Route::put('/ratings/{id}', [RatingController::class, 'update']);
+Route::delete('/ratings/{id}', [RatingController::class, 'delete']);
