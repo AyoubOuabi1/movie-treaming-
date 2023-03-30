6d616c560c19feb845ffdd0a7456bdfa38ddfdb7
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2023-03-30 17:14:30 +0200

    add categories movies table

--- movie-treaming/routes/api.php
+++ movie-treaming/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FavoriteController;
